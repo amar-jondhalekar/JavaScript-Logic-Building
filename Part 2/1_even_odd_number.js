70c6c11e101ca926e6e635d3f554831e68577469
Amar Jondhalekar <amarjondhalekar221297@gmail.com>
2025-01-05 10:35:22 +0530

    using ternary operator

--- Part 2/1_even_odd_number.js
+++ Part 2/1_even_odd_number.js
@@ -1,10 +1,18 @@
 // Write a function that tells if a given number is even or odd
 function evenOdd(num) {
-  if (num % 2 == 0) {
-    console.log("Even Number is:", num);
-  } else {
-    console.log("Odd Number is:", num);
-  }
+  return num % 2 === 0 ? "even" : "odd";
+  // if (num % 2 == 0) {
+  //   console.log("Even Number is:", num);
+  // } else {
+  //   console.log("Odd Number is:", num);
+  // }
 }
 
-evenOdd(-2);
+console.log(evenOdd(-2));
+console.log(evenOdd(0));
+console.log(evenOdd(1));
+console.log(evenOdd(153));
+console.log(evenOdd(599));
+console.log(evenOdd(1003));
+console.log(evenOdd(2.7));
+console.log(evenOdd(-0));
