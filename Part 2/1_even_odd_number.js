08fef7d7ec80b89329a4fedd66a127bd7e6f4fbe
Amar Jondhalekar <amarjondhalekar221297@gmail.com>
2025-01-05 10:23:02 +0530

    Write a function that tells if a given number is even or odd

---
+++ Part 2/1_even_odd_number.js
@@ -0,0 +1,10 @@
+// Write a function that tells if a given number is even or odd
+function evenOdd(num) {
+  if (num % 2 == 0) {
+    console.log("Even Number is:", num);
+  } else {
+    console.log("Odd Number is:", num);
+  }
+}
+
+evenOdd(-2);
