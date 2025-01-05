0ce60a45045597af9fbeb1d7f4eaa56b628575b0
Amar Jondhalekar <amarjondhalekar221297@gmail.com>
2025-01-05 13:52:49 +0530

    Write a function that calculate factorial of a given number

---
+++ Part 4/1_factorial.js
@@ -0,0 +1,11 @@
+// Write a function that calculate factorial of a given number
+// Input: A number, positive integer -> 0, 1, 4, 7, ...
+// Return: Factorial of that number
+// Factorial: Multiplication of all positive integers from 1 to a given number
+
+// Example:
+// 1!: 1
+// 2!: 2 * 1 = 2
+// 3!: 3 * 2 * 1 = 6
+// 4!: 4 * 3 * 2 * 1 = 24
+// 0!: 1
