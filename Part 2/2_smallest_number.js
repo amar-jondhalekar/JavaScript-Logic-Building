84cc95f675cdd5cf6361ca28ee25c78e474520f5
Amar Jondhalekar <amarjondhalekar221297@gmail.com>
2025-01-05 12:15:11 +0530

    added code different way to find smallest number

--- Part 2/2_smallest_number.js
+++ Part 2/2_smallest_number.js
@@ -17,3 +17,21 @@ smallestNum(7, 1, 5);
 smallestNum(6, 2, 8);
 smallestNum(9, 2, 1);
 smallestNum(0, 3, 1);
+
+function smallestNumber2(a, b, c) {
+  let smallestNumber = a;
+  if (b < smallestNumber) {
+    smallestNumber = b;
+  }
+  if (c < smallestNumber) {
+    smallestNumber = c;
+  }
+  return smallestNumber;
+}
+
+smallestNumber2(1, 2, 3);
+smallestNumber2(4, 2, 3);
+smallestNumber2(7, 1, 5);
+smallestNumber2(6, 2, 8);
+smallestNumber2(9, 2, 1);
+smallestNumber2(0, 3, 1);
