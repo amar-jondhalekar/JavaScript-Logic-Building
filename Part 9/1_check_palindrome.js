61f18e90b67332cc0c8de9b35b1f9ceda25e4a49
Amar Jondhalekar <amarjondhalekar221297@gmail.com>
2025-01-07 12:47:23 +0530

    2nd way:while loop

--- Part 9/1_check_palindrome.js
+++ Part 9/1_check_palindrome.js
@@ -1,1 +1,41 @@
 // Write a function that checks if a string is palindrome or not
+// Input: A string
+// Return: boolean, true if Palindrome and false if not palindrome
+// A palindrome is a word that reads the same backwardor forward
+
+function isPalindrome(word) {
+  let str = word.toString();
+  let reverse = str.split("").reverse().join("");
+  //   console.log(reverse);
+  return str === reverse ? true : false;
+  //   if (word === reverse) {
+  //     return true;
+  //   }
+  //   return false;
+}
+
+console.log(isPalindrome("amarama"));
+console.log(isPalindrome("jondhalekar"));
+console.log(isPalindrome("kook"));
+console.log(isPalindrome(12321));
+console.log(isPalindrome(123));
+console.log(isPalindrome("12321amar"));
+
+// 2nd way:while loop
+function isPalindrome1(word) {
+  let str = word.toString();
+  let left = 0; // start pointer
+  let right = str.length - 1; // endpointer
+
+  while (left < right) {
+    // compare characters at the pointers
+    if (str[left] !== str[right]) {
+      return false; // Not a palindrome
+    }
+    left++;
+    right--;
+  }
+  return true; // It's a palindrome
+}
+console.log("Second way: ");
+console.log(isPalindrome1("amarama"));
