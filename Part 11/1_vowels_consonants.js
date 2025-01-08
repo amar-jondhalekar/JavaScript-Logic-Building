5851985ad16f5dc18073188b9f642ee48893bf6c
Amar Jondhalekar <amarjondhalekar221297@gmail.com>
2025-01-08 12:01:46 +0530

    2nd Way:Detailed log

--- Part 11/1_vowels_consonants.js
+++ Part 11/1_vowels_consonants.js
@@ -1,4 +1,12 @@
 // Write a function that counts and prints the number of vowels and consonants in a given string
+// Input: String
+// Return: Vowel and Consonant count
+// Vowel: a e i o u
+// Consonant: All other character
+// Example:
+// "Hello World => 3 Vowels & 7 Consonants"
+
+// 1st Way:
 function countVowelAndConsonant(inputStr) {
   const vowel = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
   let vowelCount = 0;
@@ -20,3 +28,26 @@ function countVowelAndConsonant(inputStr) {
 
 console.log(countVowelAndConsonant("Hello World!"));
 console.log(countVowelAndConsonant("Amar Jondhalekar"));
+
+// 2nd Way:
+
+function countVowelAndCon(inputStr) {
+  inputStr = inputStr.toLowerCase();
+  inputStr = inputStr.replace(" ", "");
+  console.log("inputString after clean up is:", inputStr);
+  const vowel = "aeiou";
+  const consonant = "bcdfghjklmnpqrstvwxyz";
+  let vowelCount = 0;
+  let consonantCount = 0;
+  for (let i = 0; i <= inputStr.length; i++) {
+    console.log(inputStr[i]);
+    if (vowel.includes(inputStr[i])) {
+      vowelCount++;
+    } else if (consonant.includes(inputStr[i])) {
+      consonantCount++;
+    }
+  }
+  return { vowelCount, consonantCount };
+}
+
+countVowelAndCon("Amar Jondhalekar");
