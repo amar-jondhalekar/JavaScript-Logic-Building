71c9ef6e830a007ca1344575c0cf2aad64f260a6
Amar Jondhalekar <amarjondhalekar221297@gmail.com>
2025-01-10 17:42:19 +0530

    Write a function that converts a temperature in Celsius to Fahrenhite

---
+++ Part 17/1_temp_celsiusToFahrenheit.js
@@ -0,0 +1,20 @@
+// Write a function that converts a temperature in Celsius to Fahrenhite
+// Input: Temperature in Celsius
+// Return: Temperature in Fahrenheit
+// - Celsius & Fahrenhite are 2 different scales two measure temperature
+// - Formula (Temperature in degrees Celsius (C) * 9 / 5 + 32)
+// Example:
+// - 27 degrees = (27 * 9 / 5) + 32 = 80.6 Fahrenheit
+// - 34 degrees = (34 * 9 / 5) + 32 = 93.2 Fahrenheit
+function celsiusToFahrenheit(degree) {
+  let result = degree * (9 / 5);
+  let fahrenheit = result + 32;
+  return fahrenheit;
+}
+
+console.log(celsiusToFahrenheit(32));
+console.log(celsiusToFahrenheit(27));
+console.log(celsiusToFahrenheit(34));
+console.log(celsiusToFahrenheit(12));
+console.log(celsiusToFahrenheit(17));
+console.log(celsiusToFahrenheit(23));
