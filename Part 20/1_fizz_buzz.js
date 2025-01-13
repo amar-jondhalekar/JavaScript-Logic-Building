c63151be09221eabb97bc3a8c0684aeaf445c1a3
Amar Jondhalekar <amarjondhalekar221297@gmail.com>
2025-01-13 15:48:34 +0530

    Write a program that prints numbers from 1 to N, replacing

---
+++ Part 20/1_fizz_buzz.js
@@ -0,0 +1,11 @@
+// Write a program that prints numbers from 1 to N, replacing
+// - multiples of 3 with "Fizz"
+// - multiples of 5 with "Buzz"
+// - multiples of both 3 & 5 with "FizzBuzz"
+// Input: A positive number
+
+Example: 36;
+// - 1, 2, Fizz, 4, Bizz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz,
+//   13, 14, Fizz Buzz, 16, 17, Fizz, 19, Buzz, Fizz, 22,
+//   23, Fizz, Buzz, 26, Fizz, 28, 29, Fizz Buzz, 31, 32,
+//   Fizz, 34, Buzz, Fizz
